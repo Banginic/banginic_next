028e42f9119db52099ddb9456c55e9ee8aabf71f
Banginic <borisndoh@gmail.com>
2025-07-13 11:47:40 +0100

    Adjust folder structure

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function error() {
   return (
     <div>
-      
+      error occurred
     </div>
   )
 }
 
-export default Home
+export default error
