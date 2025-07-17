5ed8e5422ecf85b2f981ed95ecbddb73255e1fe3
Banginic <borisndoh@gmail.com>
2025-07-17 12:33:07 +0100

    Add  cookies

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import AppProvider from "@/context/AppProvider";
 import { client } from "@/components/userClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import News from "@/components/News";
+import {News, Cookies} from "@/components/exportComp";
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -14,9 +14,11 @@ function MainLayout({ children }: { children: React.ReactNode }) {
           <div
             className={`relative min-h-screen bg-gradient-to-br  from-slate-900 via-purple-900 to-slate-900 text-white 
       `}
-          ><News />
+          >
+            <News />
             <Header />
             {children}
+            <Cookies />
           </div>
         </QueryClientProvider>
       </AppProvider>
--- components/Cookies.tsx
+++ components/Cookies.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useState } from "react";
 
 function Cookies() {
@@ -25,21 +26,21 @@ function Cookies() {
     <section className="fixed bottom-4 -translate-x-1/2 left-1/2 z-50">
       {displayNotice && (
         <div className="w-sm lg:w-xl bg-white  dark:bg-black p-4 rounded text-sm mx-auto">
-          <p className="mb-2 text-gray-600 dark:text-gray-500">
+          <p className="mb-2 text-neutral-200 ">
             Our website uses cookies to make you have the best and smooth user
             experience on our website.
           </p>
           <button
             title="Accept cookies"
             onClick={acceptCookie}
-            className="bg-black border dark:border-accent dark:text-accent text-white px-6 py-1 rounded my-2 cursor-pointer hover:bg-black/90"
+            className="bg-black border  text-white px-6 py-2 hover:scale-x-105 trans rounded my-2 cursor-pointer hover:bg-black/90"
           >
             I'm okay with that
           </button>
           <button
             title="Reject cookies"
             onClick={declineCookie}
-            className="ml-2 px-6 py-1 text-black border rounded text-sm cursor-pointer"
+            className="ml-2 px-6 py-2 dark:border-accent dark:text-accent border rounded text-sm cursor-pointer hover:scale-x-105 trans"
           >
             No! Thank you
           </button>
